7784171d3cf6c64b994e60c47cdea3ba9f8d8fea
Surgurator <a9191084222@gmail.com>
2022-11-12 04:36:00 +0300

    pre release

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,20 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var redLight: UIView!
-    
     @IBOutlet var yellowLight: UIView!
-    
     @IBOutlet var greenLight: UIView!
     
     @IBOutlet var buttonSwith: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = redLight.frame.size.width/2
+
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        redLight.layer.cornerRadius = yellowLight.frame.size.width/2
         yellowLight.layer.cornerRadius = yellowLight.frame.size.width/2
         greenLight.layer.cornerRadius = greenLight.frame.size.width/2
     }
-    
     @IBAction func buttonSwitchPressed(_ sender: UIButton) {
         if greenLight.alpha == 1{
             redLight.alpha = 1
@@ -39,5 +40,3 @@ class ViewController: UIViewController {
         }
     }
 }
-
-
