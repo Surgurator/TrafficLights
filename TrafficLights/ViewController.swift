9a8ebecd3cd1c2c4ecef6cf580a0938f01554f63
Surgurator <a9191084222@gmail.com>
2022-11-12 14:44:14 +0300

    Final Release 1.1

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -13,30 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet var greenLight: UIView!
     
     @IBOutlet var buttonSwith: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        redLight.alpha = 0.5
+        yellowLight.alpha = 0.5
+        greenLight.alpha = 0.5
     }
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         redLight.layer.cornerRadius = yellowLight.frame.size.width/2
         yellowLight.layer.cornerRadius = yellowLight.frame.size.width/2
         greenLight.layer.cornerRadius = greenLight.frame.size.width/2
     }
+    
     @IBAction func buttonSwitchPressed(_ sender: UIButton) {
-        if greenLight.alpha == 1{
+        
+        switch (redLight.alpha, yellowLight.alpha, greenLight.alpha) {
+        case (0.5, 0.5, 0.5):
             redLight.alpha = 1
             yellowLight.alpha = 0.5
             greenLight.alpha = 0.5
-        } else if redLight.alpha == 1{
+            buttonSwith.setTitle("Next", for: .normal)
+        case (1, 0.5, 0.5):
             redLight.alpha = 0.5
             yellowLight.alpha = 1
             greenLight.alpha = 0.5
-        } else if yellowLight.alpha == 1{
+        case (0.5, 1, 0.5):
             redLight.alpha = 0.5
             yellowLight.alpha = 0.5
             greenLight.alpha = 1
+        case (0.5, 0.5, 1):
+            redLight.alpha = 1
+            yellowLight.alpha = 0.5
+            greenLight.alpha = 0.5
+        default:
+            print("error")
         }
     }
 }
